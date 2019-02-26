3a6d22f4c5791e6a7b872a0626a29b44cef41af9
shirin <shirinanlen@gmail.com>
2019-02-25 21:02:50 -0500

    some notes

--- gif-player/src/components/KeyboardDiv.jsx
+++ gif-player/src/components/KeyboardDiv.jsx
@@ -6,6 +6,12 @@ return (
   <div className="Keyboard-div">
     {keyboard.map(gifBox => (
       <div>{gifBox.keyName}</div>
+
+      //onClink in div-> and then connect it to
+      //the app.js
+      //to what am i connecting it in the keyboard??
+      //and then display the Gif list here.
+        
     ))}
   </div>
   )
