c9a3b5ab9f2895b2b7eb0c7d6cc0f506263764cd
shirin <shirinanlen@gmail.com>
2019-03-01 07:28:09 -0500

    mediaq

--- gif-player/src/components/Again.jsx
+++ gif-player/src/components/Again.jsx
@@ -4,7 +4,7 @@ const Again = (props) => {
   return (
     <div>
     <p className='done'> Now just play and hit your keys! </p>
-    <button onClick={props.refreshPage}> Again? </button>
+    <button className='again' onClick={props.refreshPage}> Again? </button>
     </div>
   )
 }
