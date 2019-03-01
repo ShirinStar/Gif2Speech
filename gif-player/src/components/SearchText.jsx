fa66ef89d93dfa6c8805c21a4c8f061e4cc270e5
shirin <shirinanlen@gmail.com>
2019-02-28 21:46:57 -0500

    cleaning code

--- gif-player/src/components/SearchText.jsx
+++ gif-player/src/components/SearchText.jsx
@@ -10,30 +10,30 @@ export class SearchText extends Component {
     this.onSubmit = this.onSubmit.bind(this);
   }
 
-  onChange = (e) => this.setState({
+    onChange = (e) => this.setState({
     [e.target.name]: e.target.value
   });
 
-  onSubmit = (e) => {
-    e.preventDefault();
-    const { value } = e.target;
-    this.props.searchText(this.state.value);
-    this.setState({ value: '' })
-    this.props.history.push('/');
+    onSubmit = (e) => {
+      e.preventDefault();
+      const { value } = e.target;
+      this.props.searchText(this.state.value);
+      this.setState({ value: '' })
+      this.props.history.push('/');
   }
 
   render() {
     return(
-  <div>
-    <form className="mainForm" onSubmit={this.onSubmit}>
-    <input className ="typeGif"
-      type='text'
-      name='value'
-      placeholder='type something...'
-      autoComplete='off'
-      value={this.state.value}
-      onChange={this.onChange}
-    />
+    <div>
+      <form className="mainForm" onSubmit={this.onSubmit}>
+        <input className ="typeGif"
+          type='text'
+          name='value'
+          placeholder='type something...'
+          autoComplete='off'
+          value={this.state.value}
+          onChange={this.onChange}
+        />
     </form>
   </div>
     )
